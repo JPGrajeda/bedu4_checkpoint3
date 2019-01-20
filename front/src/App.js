c1c8351edb054a56d02955e98aa7d2f28321fe97
veronica <veronicaoviedolopez@gmail.com>
2019-01-20 16:38:50 -0600

    graficos
    los graficos

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/Main/Login';
+import Graphics from './components/Main/graphics';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Login></Login>
+        <Graphics/>
       </div>
     );
   }
