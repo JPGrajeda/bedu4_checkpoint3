fe194a1461038e71cede8d5bbe003394bb748c8c
veronica <veronicaoviedolopez@gmail.com>
2019-01-23 18:33:14 -0600

    RestFul Apis
    se agreganlosi endpoint para conectar con mongooes

--- back/index.js
+++ back/index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3005;
 var cors = require('cors')
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const url = 'mongodb://admin:admin123@ds119772.mlab.com:19772/checkpoint3';
+const Service = require('./models/serviceModel');
+mongoose.connect(url, { useNewUrlParser: true });
+  
+
 app.use(cors());
 var arreglo  = [
     {
@@ -36,6 +42,54 @@ app.post('/autenticar',function(req, res){
         res.send('FALL');
     }
 })
+
+
+
+app.post("/addService", (req, res) => {
+    const myData = new Service(req.body);
+    myData.save()
+     .then(item => {
+     res.send('ok');
+     })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+     });
+});
+
+app.get("/getServices", async(req, res) => {
+    try{
+        const respuesta = await Service.find({});
+	    res.send(respuesta);
+    }
+    catch(err) {
+        res.status(400).send("unable to get data from database");
+    }
+});
+
+app.get("/getService/:id", async(req, res) => {
+    try{
+       
+        const respuesta = await Service.find({_id: req.params.id});
+	    res.send(respuesta);
+    }
+    catch(err) {
+        res.status(400).send("unable to get data from database");
+    }
+});
+
+app.get("/updateServicePrice/:id", async(req, res) => {
+    try{
+        const respuesta = await Service.findOneAndUpdate({_id: req.params.id},
+            { "price": 640},
+            { new: true }
+        ).exec();
+        
+	    res.send(respuesta);
+    }
+    catch(err) {
+        res.status(400).send("unable to update data from database");
+    }
+});
 
 app.listen(port, function(){
     console.log('servidor funcionando correcto en el puerto:', port);
