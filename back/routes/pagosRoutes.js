d81a85052adf5245e1222fc745b9b6bf14562481
Pablo Grajeda <grajeda.gragon@gmail.com>
2019-02-22 18:02:56 -0600

    Peticion pagos

--- back/routes/pagosRoutes.js
+++ back/routes/pagosRoutes.js
@@ -18,14 +18,52 @@ module.exports = (app) => {
     // GET PAGOS POR TIPO
     app.get('/api/pagosTipo', async (req, res) => {
         try {
-            // const response = await Pago.find({}).select({ servicio: 1 });
+            // const response = await Pago.find({}).select({ servicio: 1 }); // trae todo los servicios pagados
             // const response = await Pago.find({})
             //                            .select({ 
             //                                _id: 0,
             //                                'servicio.nombre': 1, 
             //                                'servicio.importe': 1,
             //                                'servicio.tipo': 1
-            //                             });
+            //                             }); // trae el detalle de servicios pagados
+            // const response = await Pago.aggregate(
+            //     [
+            //       { 
+            //           $group : { 
+            //               _id: "$id_tarjeta" ,
+            //               total: { $sum: '$total' }
+            //             } 
+            //       }
+            //     ]
+            //  ); // trae el total de pago por tarjeta
+
+
+            // const response = await Pago.aggregate(
+            //     [
+            //       { 
+            //           $group : { 
+            //               _id: { tarjeta: "$id_tarjeta", servicio: "$servicio.nombre" },
+            //               sum: { $sum: '$servicio.importe'}
+            //             } 
+            //       }
+            //     ]
+            //  );
+             const response = await Pago.aggregate(
+                [
+                  {
+                    $group : {
+                        _id: { tarjeta: "$id_tarjeta" , nombre: '$servicio.nombre', importe: '$servicio.importe' },
+                        sum: { $sum: '$servicio.importe' }
+                    } 
+                  },
+                //   {
+                //     $project: {
+                //         'servicio.nombre': 0
+                //     }
+                //   }
+                ]
+             );
+
             res.send(response);
         } catch (error) {
             console.log('error api pagos');
